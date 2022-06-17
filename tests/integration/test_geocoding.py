962683edb45f013ec917bc853728b6ef1ebc83f2
siida36 <siida0118@gmail.com>
2022-06-17 09:42:44 +0900

    Fix to use mocker

--- tests/integration/test_geocoding.py
+++ tests/integration/test_geocoding.py
@@ -1,15 +1,23 @@
 import pytest
+
 from smart_sauna_map.api import app
 
 
 @pytest.mark.parametrize(
-    "query, expected_status, expected_res", [
+    "query, expected_status, expected_res",
+    [
         (
-            "新宿", 200, {'lat': 35.6938253, 'lng': 139.7033559},
+            "新宿",
+            200,
+            {"lat": 35.6938253, "lng": 139.7033559},
         ),
-    ]
+    ],
 )
-def test_normal(query, expected_status, expected_res):
+def test_normal(mocker, query, expected_status, expected_res):
+    mocker.patch(
+        "smart_sauna_map.geocoding.geocode",
+        return_value=({"lat": 35.6938253, "lng": 139.7033559}, 200),
+    )
     rv = app.test_client().post("/", json={"query": query})
     data = rv.get_json()
     for key in expected_res:
@@ -18,13 +26,20 @@ def test_normal(query, expected_status, expected_res):
 
 
 @pytest.mark.parametrize(
-    "query, expected_status, expected_res", [
+    "query, expected_status, expected_res",
+    [
         (
-            "", 404, None,
+            "",
+            404,
+            None,
         ),
-    ]
+    ],
 )
-def test_abnormal(query, expected_status, expected_res):
+def test_abnormal(mocker, query, expected_status, expected_res):
+    mocker.patch(
+        "smart_sauna_map.geocoding.geocode",
+        return_value=({"lat": None, "lng": None}, 404),
+    )
     rv = app.test_client().post("/", json={"query": query})
     data = rv.get_json()
     assert rv.status_code == expected_status
